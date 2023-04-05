2c4f4a3a0863c6d70b359715ef87bd70e7c711f9
chadaevw <vitalii.chadaev@grphn.ru>
2023-04-05 10:24:42 +0400

    added polyfil to IE11

--- src/js/internal.js
+++ src/js/internal.js
@@ -3,6 +3,7 @@
   function documentReady() {
     @@include('partials/add-class.js');
     @@include('partials/objectFit.js');
+    @@include('partials/closest.js');
   };
 
   document.addEventListener("DOMContentLoaded", documentReady);
---
+++ src/js/partials/closest.js
@@ -0,0 +1,28 @@
+(function (e) {
+    e.closest = e.closest || function (css) {
+        var node = this;
+        while (node) {
+            if (node.matches(css)) {
+                return node;
+            } else {
+                node = node.parentElement;
+            }
+        }
+        return null;
+    };
+})(Element.prototype);
+
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+    Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector ||
+    Element.prototype.webkitMatchesSelector ||
+    function (s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+            i = matches.length;
+        while (--i >= 0 && matches.item(i) !== this) {}
+        return i > -1;
+    };
+}
